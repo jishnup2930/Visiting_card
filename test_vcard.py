61fe71a59de9af7f0cbd9cb1eb824e8a750bd1ed
jishnu <jishnup15@gmail.com>
2023-11-14 21:28:53 +0530

    Green: Added test case to check input data and generate vcards

--- test_vcard.py
+++ test_vcard.py
@@ -1,26 +1,31 @@
-from gen_vcard import generate_vcard
 import os
+from gen_vcard import input_data,generate_vcards
 
-def test_gen_vcard():
-    with open('temp.csv', 'w') as file:
-        file.write('Acosta, Monica, Lawyer, monic.acost@barnes-stewwart.org, 813.771.3464x71941')
-    generate_vcard('temp.csv')
-    with open ('vcard/AcostaMonica.vcf','r') as f:
+def test_get_data():
+    test_file = "/tmp/sample.csv"
+    with open(test_file, 'w') as f:
+        f.write('John,Bob,Lawyer,johnbob@example.com,55454616545\n')
+    data = input_data(test_file)
+    assert data == [['John','Bob','Lawyer','johnbob@example.com','55454616545']]
+    os.unlink(test_file)
+
+def test_generate_vcards():
+    data = [['John','Bob','Lawyer','johnbob@example.com','55454616545']]
+    generate_vcards(data)
+    with open('./vcards/JohnBob.vcf', 'r') as f:
         vcard = f.read()
-
-        assert os.path.exists('vcard/AcostaMonica.vcf')
-
-        expected_output =("""
+    assert os.path.exists('./vcards/JohnBob.vcf')
+    assert f"""
 BEGIN:VCARD
 VERSION:2.1
-Name: Monica;Acosta
-FullName: Acosta Monica
+N: Bob;John
+FN: John Bob
 ORG:Authors, Inc.
 TITLE: Lawyer
-TEL;WORK;VOICE: 813.771.3464x71941
+TEL;WORK;VOICE: 55454616545
 ADR;WORK: 100 Flat Grape Dr.;Fresno;CA;95555;United States of America
-EMAIL;PREF;INTERNET: monic.acost@barnes-stewart.org
+EMAIL;PREF;INTERNET: johnbob@example.com
 REV:20150922T195243Z
 END:VCARD
-""")
-    assert vcard == expected_output
+""" in vcard
+    os.unlink('./vcards/JohnBob.vcf')
