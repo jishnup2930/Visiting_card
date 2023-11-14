98fb4cdd4234308675bb903465510aeda9c7ed65
jishnu <jishnup15@gmail.com>
2023-11-14 10:51:38 +0530

    Green:Replaced csv reading to write into a file to pass the test
    Added new lines to check the path of saved vcard, and asserted the saved vacrd with expected
    vcard

--- test_vcard.py
+++ test_vcard.py
@@ -1,13 +1,14 @@
 from gen_vcard import generate_vcard
-import csv
+import os
 
 def test_gen_vcard():
-    # input_data = [['Acosta', 'Monica', 'Lawyer', 'monic.acost@barnes-stewwart.org', '813.771.3464x71941']]
-    with open('temp.csv', 'r') as file:
-        reader = csv.reader(file)
-        for i in reader:
-            return i
-        actual_output = generate_vcard(i)
+    with open('temp.csv', 'w') as file:
+        file.write('Acosta, Monica, Lawyer, monic.acost@barnes-stewwart.org, 813.771.3464x71941')
+    generate_vcard('temp.csv')
+    with open ('vcard/AcostaMonica.vcf','r') as f:
+        vcard = f.read()
+
+        assert os.path.exists('vcard/AcostaMonica.vcf')
 
         expected_output =("""
 BEGIN:VCARD
@@ -22,4 +23,4 @@ EMAIL;PREF;INTERNET: monic.acost@barnes-stewart.org
 REV:20150922T195243Z
 END:VCARD
 """)
-    assert actual_output == expected_output
+    assert vcard == expected_output
