14b9df18b0f9b251790f74a98010fb6f3505dffb
jishnu <jishnup15@gmail.com>
2023-11-17 12:17:46 +0530

    Refactored code for cleanliness and clarity

--- gen_vcard.py
+++ gen_vcard.py
@@ -4,92 +4,92 @@ import logging
 import os
 import requests
 
-logger = None
-
 def parse_args():
-    parser = argparse.ArgumentParser(prog="gen_vcard.py",
-                                     description="Generates employee visiting card and qr code")
-    parser.add_argument("ipfile",help='Name of input file')
+    parser = argparse.ArgumentParser(prog="gen_vcard.py", description="Generates employee visiting card and QR code")
+    parser.add_argument("ipfile", help='Name of input file')
     parser.add_argument("-v", "--verbose", help="Print detailed logging", action='store_true', default=False)
     parser.add_argument("-n", "--number", help="Number of vcards to generate", action='store', type=int, default=10)
-    parser.add_argument("-q","--qrcode", help ="Generate vacrd and QR code ",action ='store_true')
-    parser.add_argument("-s","--size", help ="Size of QR code [100-500]",action ='store',type=int,default=500)
-    parser.add_argument("-a","--address",help="To give the address in vcard",action='store',default='100 Flat Grape Dr.;Fresno;CA;95555;United States of America')
-    args = parser.parse_args()
-    return args
-                
+    parser.add_argument("-q", "--qrcode", help="Generate vcards and QR codes", action='store_true')
+    parser.add_argument("-s", "--size", help="Size of QR code [100-500]", action='store', type=int, default=500)
+    parser.add_argument("-a", "--address", help="Address in vcard", action='store',
+                        default='100 Flat Grape Dr.;Fresno;CA;95555;United States of America')
+    return parser.parse_args()
+
+logger = None
 def setup_logging(log_level):
     global logger
     logger = logging.getLogger("vcard")
-    handler = logging.StreamHandler()
-    fhandler = logging.FileHandler("run.log")
-    fhandler.setLevel(logging.DEBUG)
-    handler.setLevel(log_level)
-    handler.setFormatter(logging.Formatter("[%(levelname)s] %(asctime)s | %(filename)s:%(lineno)d | %(message)s"))
-    fhandler.setFormatter(logging.Formatter("[%(levelname)s] %(asctime)s | %(filename)s:%(lineno)d | %(message)s"))
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
-    logger.addHandler(fhandler)
-    
-def parse_data(file):
-    data = []
-    with open(file, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            data.append(row)
-    return data
+    formatter = logging.Formatter("[%(levelname)s] %(asctime)s | %(filename)s:%(lineno)d | %(message)s")
 
-def gen_vcard_content(first_name, last_name, title, email, phone_number,address):
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(log_level)
+    console_handler.setFormatter(formatter)
+
+    file_handler = logging.FileHandler('run.log')
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+    return logger
+
+def read_data(file):
+    with open(file, 'r') as input_file:
+        reader = csv.reader(input_file)
+        return list(reader)
+
+def generate_vcard_content(first_name, last_name, title, email, phone_number, address):
     vcard = f"""
-BEGIN:VCARD
-VERSION:2.1
-N: {last_name};{first_name}
-FN: {first_name} {last_name}
-ORG:Authors, Inc.
-TITLE: {title}
-TEL;WORK;VOICE: {phone_number}
-ADR;WORK: {address}
-EMAIL;PREF;INTERNET: {email}
-REV:20150922T195243Z
-END:VCARD
-"""
+    BEGIN:VCARD
+    VERSION:2.1
+    N: {last_name};{first_name}
+    FN: {first_name} {last_name}
+    ORG:Authors, Inc.
+    TITLE: {title}
+    TEL;WORK;VOICE: {phone_number}
+    ADR;WORK: {address}
+    EMAIL;PREF;INTERNET: {email}
+    REV:20150922T195243Z
+    END:VCARD
+    """
     return vcard
 
-def generate_all_vcards(data,number,address):
-    if not os.path.exists('vcards'):   
+def generate_vcards(data, number, address):
+    if not os.path.exists('vcards'):
         os.mkdir('vcards')
-    count = 0
-    for i in data:
-        count +=1
-        first_name, last_name, title, email, phone_number = i
-        vcard=gen_vcard_content(first_name, last_name, title, email, phone_number,address)
-
-        with open(f'vcards/{first_name}{last_name}.vcf', 'w') as f:
-            f.write(vcard)
-            logger.debug("%d Generated vcard for %s ", count ,first_name)
-        if count >=number:
-            break
-    logger.info("Vcard generated successfully")
-
-def gen_qrcode_content(vcard,size):
-    qrcode = requests.get(f'https://chart.googleapis.com/chart?cht=qr&chs={size}x{size}&chl={vcard}')
-    qr = qrcode.content 
-    return qr
     
-def generate_all_qrcode(qr,number,size): 
-    if not os.path.exists('vcards'):   
-        os.mkdir('vcards') 
     count = 0
-    for data in qr:
-        count +=1
-        first_name, last_name, title, email, phone_number = data
-
-        with open(f'vcards/{first_name}{last_name}.qr.png', 'wb') as file:
-            file.write(gen_qrcode_content(data,size))
-            logger.debug("%d Generated QR code for %s ", count ,first_name)
-        if count >=number:
+    for employee in data:
+        count += 1
+        first_name, last_name, title, email, phone_number = employee
+        vcard_content = generate_vcard_content(first_name, last_name, title, email, phone_number, address)
+        vcard_filename = f'{first_name}{last_name}.vcf'
+        with open(os.path.join('vcards', vcard_filename), 'w') as f:
+            f.write(vcard_content)
+            logger.debug("%d Generated vcard for %s ", count, first_name)
+        if count >= number:
             break
-    logger.info("QR code generated successfully")
+    logger.info("Vcard generation completed")
+
+def generate_qrcode_content(vcard, size):
+    return requests.get(f'https://chart.googleapis.com/chart?cht=qr&chs={size}x{size}&chl={vcard}').content
+
+def generate_qrcodes(data, number, size):
+    if not os.path.exists('vcards'):
+        os.mkdir('vcards')
+
+    count = 0
+    for employee_data in data:
+        count += 1
+        first_name, last_name, title, email, phone_number = employee_data
+        qr_filename = f'{first_name}{last_name}.qr.png'
+        with open(os.path.join('vcards', qr_filename), 'wb') as file:
+            file.write(generate_qrcode_content(employee_data, size))
+            logger.debug("%d Generated QR code for %s ", count, first_name)
+        if count >= number:
+            break
+    logger.info("QR code generation completed")
 
 def main():
     args = parse_args()
@@ -97,14 +97,12 @@ def main():
         setup_logging(logging.DEBUG)
     else:
         setup_logging(logging.INFO)
-    file = args.ipfile
-    data = parse_data(file)
+    data = read_data(args.ipfile)
     if args.qrcode:
-        if args.size:
-            generate_all_vcards(data,args.number,args.address)   
-            generate_all_qrcode(data,args.number,args.size)
+        generate_vcards(data, args.number, args.address)
+        generate_qrcodes(data, args.number, args.size)
     else:
-        generate_all_vcards(data,args.number,args.address)
+        generate_vcards(data, args.number, args.address)
 
 if __name__ == '__main__':
     main()
--- test_vcard.py
+++ test_vcard.py
@@ -1,32 +1,32 @@
 import os
 import requests
-from gen_vcard import parse_data,gen_vcard_content,gen_qrcode_content
+from gen_vcard import read_data,generate_vcard_content,generate_qrcode_content
 
 def test_get_data():
     test_file = "/tmp/sample.csv"
     with open(test_file, 'w') as f:
         f.write('John,Bob,Lawyer,johnbob@example.com,55454616545\n')
-    data = parse_data(test_file)
+    data = read_data(test_file)
     assert data == [['John','Bob','Lawyer','johnbob@example.com','55454616545']]
     os.unlink(test_file)
 
-def test_vcard_content(): 
-    data = gen_vcard_content('John','Bob','Lawyer','johnbob@example.com','55454616545','100 Flat Grape Dr.;Fresno;CA;95555;United States of America')
+def test_generate_vcard_content(): 
+    data = generate_vcard_content('John','Bob','Lawyer','johnbob@example.com','55454616545','100 Flat Grape Dr.;Fresno;CA;95555;United States of America')
     assert data == """
-BEGIN:VCARD
-VERSION:2.1
-N: Bob;John
-FN: John Bob
-ORG:Authors, Inc.
-TITLE: Lawyer
-TEL;WORK;VOICE: 55454616545
-ADR;WORK: 100 Flat Grape Dr.;Fresno;CA;95555;United States of America
-EMAIL;PREF;INTERNET: johnbob@example.com
-REV:20150922T195243Z
-END:VCARD
-"""
+    BEGIN:VCARD
+    VERSION:2.1
+    N: Bob;John
+    FN: John Bob
+    ORG:Authors, Inc.
+    TITLE: Lawyer
+    TEL;WORK;VOICE: 55454616545
+    ADR;WORK: 100 Flat Grape Dr.;Fresno;CA;95555;United States of America
+    EMAIL;PREF;INTERNET: johnbob@example.com
+    REV:20150922T195243Z
+    END:VCARD
+    """
 
-def test_gen_qrcode_content():
+def test_generate_qrcode_content():
     size = 500
     vcard = """
 BEGIN:VCARD
@@ -44,6 +44,6 @@ END:VCARD
     expected_url = requests.get(f'https://chart.googleapis.com/chart?cht=qr&chs={size}x{size}&chl={vcard}')
     expected_qr_content = expected_url.content
 
-    generated_qr = gen_qrcode_content(vcard, size)
+    generated_qr = generate_qrcode_content(vcard, size)
 
     assert generated_qr == expected_qr_content
