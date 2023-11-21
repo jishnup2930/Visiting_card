4ce3b25a65bed9b0654dbb1b75fd3f00bc51e659
jishnu <jishnup15@gmail.com>
2023-11-22 00:21:09 +0530

    Implemented new functions to fetch leave table and count of leave remaining for a employee
    Code refactored for cleanliness and clarity

--- gen_vcard.py
+++ gen_vcard.py
@@ -6,10 +6,11 @@ import psycopg2
 from psycopg2.extensions import AsIs
 import requests
 
+logger = None
+
 def parse_args():
     parser = argparse.ArgumentParser(prog="gen_vcard.py", description="Generates employee visiting card and QR code")
     parser.add_argument("-v", "--verbose", help="Print detailed logging", action='store_true', default=False)
-
     subparsers = parser.add_subparsers(dest='subcommand', help='Subcommand')
 
     parser_init = subparsers.add_parser('initdb', help='Initialize database')
@@ -33,9 +34,23 @@ def parse_args():
     parser_vcard.add_argument('-d', '--database', help='Database name', action='store', default='hr')
     parser_vcard.add_argument('-t','--table',help = 'Table name', action = 'store',default ='employees')
 
-    return parser.parse_args()
+    parser_insert = subparsers.add_parser('leave',help = "Insert into leave table")
+    parser_insert.add_argument('employee_id',help = "Employee_id")
+    parser_insert.add_argument('date',help = "Leave Date")
+    parser_insert.add_argument('reason',help = "Reason of leave")
+    parser_insert.add_argument('-u','--user',help = 'Input user name',action = 'store',default = 'jishnu')
+    parser_insert.add_argument('-d', '--database', help='Database name', action='store', default='hr')
 
-logger = None
+    parser_fetch_leave_table = subparsers.add_parser('leave_table',help = "Fetch leave table")
+    parser_fetch_leave_table.add_argument('-d','--database',help = 'Database name',action ='store',default = 'hr')
+    parser_fetch_leave_table.add_argument('-u','--user',help = 'User name',action = 'store',default = 'jishnu')
+    
+    parser_leave_remains =subparsers.add_parser('leave_remains',help = "Leaves remain")
+    parser_leave_remains.add_argument('employee_id',help = "Employee_id")
+    parser_leave_remains.add_argument('-d', '--database', help='Database name', action='store', default='hr')
+    parser_leave_remains.add_argument('-u','--user',help = 'Input user name',action = 'store',default = 'jishnu')
+
+    return parser.parse_args()
 
 def setup_logging(log_level):
     global logger
@@ -82,39 +97,112 @@ def create_table(database,table,user):
                 firstname VARCHAR,
                 lastname VARCHAR,
                 title VARCHAR,
-                email VARCHAR,
+                email VARCHAR UNIQUE,
                 phone_number VARCHAR
             );""",(AsIs(table),))
         conn.commit()
+        conn.close()
         logger.info("Table %s created successfully",table)
     except Exception as e:
         logger.error("Error creating table : %s",e)
+def load_csv_to_database(csv_file, database, user, table):
+    try:
+        conn = psycopg2.connect(dbname=database, user=user)
+        cursor = conn.cursor()
+        cursor.execute("TRUNCATE TABLE %s RESTART IDENTITY CASCADE;", (AsIs(table),))
+        with open(csv_file, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                cursor.execute(f"""
+                    INSERT INTO {table} (firstname, lastname, title, email, phone_number)
+                    VALUES (%s, %s, %s, %s, %s);
+                """, row)
+        conn.commit()
+        conn.close()
+        logger.info("CSV data %s loaded into the database successfully", csv_file)
+    except Exception as e:
+        logger.error("Error loading CSV data to database: %s", e)
 
-def load_csv_to_database(csv_file,database,user,table):
+def create_leave_table(database,user,table):
     try:
         conn = psycopg2.connect(dbname=database,user=user)
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM %s;",(AsIs(table),))
-        with open(csv_file, 'r') as file:
-            reader = csv.reader(file)
-            id = 1
-            for row in reader:
-                cursor.execute(f"""
-                    INSERT INTO {table} (id, firstname, lastname, title, email, phone_number)
-                    VALUES (%s, %s, %s, %s, %s, %s);
-                """,(id,)+tuple(row))
-                id+=1
+        # cursor.execute("TRUNCATE TABLE leave_table;")
+        cursor.execute(f"""
+        CREATE TABLE IF NOT EXISTS leave_table(
+        employee_id INTEGER REFERENCES {table}(id),
+        date DATE,
+        reason VARCHAR(50),
+        PRIMARY KEY(employee_id,date));
+        """)
         conn.commit()
-        logger.info("CSV data %s loaded into the database successfully",csv_file)
+        conn.close()
+        logger.info("Leave table created successfully")
     except Exception as e:
-        logger.error("Error loading CSV data to database: %s".e)
+        logger.error(f"Error creating total leave table {e}")
+
+def insert_data_into_leave_table(database,user,employee_id,date,reason):
+    try:
+        conn = psycopg2.connect(dbname=database,user=user)
+        cursor = conn.cursor()
+        cursor.execute("""
+        INSERT INTO leave_table(
+        employee_id,date,reason)
+        VALUES (%s, %s, %s);""",(employee_id,date,reason))
+        conn.commit()
+        conn.close()
+        logger.info("Data inserted into leave_table successfully")
+    except Exception as e:
+        logger.error(f"Error updating data : {e}")
+        conn.close()
+
+def fetch_leave_table(database,user):
+    try:
+        conn = psycopg2.connect(dbname=database,user = user)
+        cursor = conn.cursor()
+        cursor.execute("""
+        SELECT * FROM leave_table ;
+        """)
+        data = cursor.fetchall()
+        logger.info("Leave data fetched successfully")
+        for row in data:
+            print(row)
+        conn.close()
+    except Exception as e:
+        logger.error(f"Error fetching table : {e}")
+
+def get_leave_remains(database, user, employee_id):
+    try:
+        TOTAL_LEAVES = 20
+        conn = psycopg2.connect(dbname=database, user=user)
+        cursor = conn.cursor()
+        cursor.execute(f"""SELECT COUNT(l.employee_id), e.firstname, e.lastname, e.id 
+        FROM employees e 
+        JOIN leave_table l ON 
+        e.id =l.employee_id where e.id = {employee_id}
+        group by e.id, e.firstname, e.lastname, l.employee_id;
+        """)     
+        leaves_data = cursor.fetchone()   
+
+        if leaves_data:
+            leaves_taken = leaves_data[0]
+            firstname = leaves_data[1]
+            lastname  = leaves_data[2]
+            leave_remaining = TOTAL_LEAVES - leaves_taken
+            conn.close()
+            logger.info(f"Leave remaining for  {firstname} {lastname} with id number {employee_id}: {leave_remaining} days")
+        else:
+            logger.info(f"Employee id {employee_id} : No leaves taken yet")
+
+    except Exception as e:
+        logger.error(f"Error fetching leave data: {e}")
 
 def fetch_data(database,user,table):
-        connection = psycopg2.connect(dbname=database, user=user)
-        cursor =connection.cursor()
-        cursor.execute("SELECT * from %s ",(AsIs(table),))
-        data = cursor.fetchall()
-        return data
+    connection = psycopg2.connect(dbname=database, user=user)
+    cursor =connection.cursor()
+    cursor.execute("SELECT * from %s ",(AsIs(table),))
+    data = cursor.fetchall()
+    return data
 
 def generate_vcard_content(first_name, last_name, title, email, phone_number, address):
     vcard = f"""
@@ -137,6 +225,8 @@ def generate_all_vcards(data, number, address):
         os.mkdir('vcards')
     count = 0
     for employee in data:
+        if count >= number:
+            break 
         count += 1
         first_name, last_name, title, email, phone_number = employee[1:]
         vcard_content = generate_vcard_content(first_name, last_name, title, email, phone_number, address)
@@ -144,8 +234,6 @@ def generate_all_vcards(data, number, address):
         with open(os.path.join('vcards', vcard_filename), 'w') as f:
             f.write(vcard_content)
             logger.debug("%d Generated vcard for %s ", count, first_name)
-        if count >= number:
-            break
     logger.info("Vcard generation completed")
 
 def generate_qrcode_content(vcard, size):
@@ -172,16 +260,28 @@ def main():
         setup_logging(logging.DEBUG)
     else:
         setup_logging(logging.INFO)
-        setup_logging(logging.ERROR)
+    
+    if args.subcommand == 'leave_table':
+        fetch_leave_table(args.database,args.user)
 
     if args.subcommand == 'initdb':
         init_database(args.database,args.user)
         create_table(args.database,args.table,args.user)
+        create_leave_table(args.database,args.user,args.table)
+    
     if args.subcommand == 'load':
         load_csv_to_database(args.ipfile,args.database,args.user,args.table)
+
+    if args.subcommand == 'leave':
+        insert_data_into_leave_table(args.database,args.user,args.employee_id,args.date,args.reason)
+
+    if args.subcommand == 'leave_remains':
+        data = get_leave_remains(args.database, args.user, args.employee_id)
+    
     if args.subcommand == 'vcard':
         data = fetch_data(args.database,args.user,args.table)
         generate_all_vcards(data,args.number,args.address)
+    
         if args.qrcode:
             generate_all_qrcodes(data,args.number,args.size)
 
