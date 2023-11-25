3a2bafd77181a88a3689629e1ae3f7df3ead7647
jishnu <jishnup15@gmail.com>
2023-11-25 18:29:56 +0530

    SQL updated after fixing the errors

--- sql/leave_count.sql
+++ sql/leave_count.sql
@@ -7,10 +7,3 @@ JOIN designation d
 ON e.designation = d.designation_name 
 WHERE e.id = %s
 GROUP BY e.id,e.fname,e.email,d.num_of_leaves,d.designation_name;
-
-
--- SELECT   d.num_of_leaves AS NUMBER, e.fname, e.lname, d.designation_name 
--- FROM designation d 
--- JOIN employee e 
--- ON d.designation_name = e.designation 
--- WHERE e.id = 1; 
