57bd90c30bd069c97928bd861ba826f39799c049
jishnu <jishnup15@gmail.com>
2023-11-25 12:12:18 +0530

    Iplememted SQL to create designation table with total leaves

--- sql/init.sql
+++ sql/init.sql
@@ -1,14 +1,17 @@
 CREATE TABLE IF NOT EXISTS designation (
-    id SERIAL PRIMARY KEY,
-    designation_name VARCHAR(50) UNIQUE NOT NULL,
-    num_of_leaves INTEGER NOT NULL
+    id SERIAL,
+    designation_name VARCHAR(150) PRIMARY KEY,
+    num_of_leaves INTEGER NOT NULL,
+    UNIQUE (id, designation_name)
 );
+
+
 
 CREATE TABLE IF NOT EXISTS employees (
     id SERIAL PRIMARY KEY,
     fname VARCHAR(50),
     lname VARCHAR(50),
-    designation VARCHAR(50) REFERENCES designation(designation_name),
+    designation VARCHAR(150) REFERENCES designation(designation_name),
     email VARCHAR(120),
     phone VARCHAR(50)
 );
@@ -21,4 +24,9 @@ CREATE TABLE IF NOT EXISTS leaves (
     UNIQUE (employee_id, date)
 );
 
-
+INSERT INTO designation (designation_name, num_of_leaves)
+VALUES  ('Staff Engineer', 20),
+        ('Senior Engineer', 18),
+        ('Junior Engineer', 12),
+        ('Tech. Lead', 12),
+        ('Project Manager', 15);
--- sql/leave_count.sql
+++ sql/leave_count.sql
@@ -1,6 +1,16 @@
-SELECT COUNT(l.employee_id), e.fname, e.lname, e.id 
-FROM employees e 
-JOIN leaves l ON e.id = l.employee_id 
+SELECT COUNT (e.id) AS count, 
+e.fname , e.lname, d.designation_name , d.num_of_leaves 
+FROM employees e
+JOIN leaves l
+ON e.id = l.employee_id 
+JOIN designation d
+ON e.designation = d.designation_name 
 WHERE e.id = %s
-GROUP BY e.id, e.fname, e.lname, l.employee_id;
+GROUP BY e.id,e.fname,e.email,d.num_of_leaves,d.designation_name;
 
+
+-- SELECT   d.num_of_leaves AS NUMBER, e.fname, e.lname, d.designation_name 
+-- FROM designation d 
+-- JOIN employee e 
+-- ON d.designation_name = e.designation 
+-- WHERE e.id = 1; 
