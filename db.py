1eec56911fb8e7273006e2a48cb576d20d307731
jishnu <jishnup15@gmail.com>
2023-11-30 21:23:43 +0530

    Implemented SQLAlchemy ORM models for HRMS database (Employee, Designation, Leave)

--- db.py
+++ db.py
@@ -5,80 +5,44 @@ from typing import List
 from sqlalchemy import String, Integer, Date, create_engine, ForeignKey, UniqueConstraint, select 
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, sessionmaker
 
-class  HRDBBase(DeclarativeBase):    
+class HRDBBase(DeclarativeBase):    
     pass
     
+class Employee(HRDBBase):
+    __tablename__='hrms_employees'
+    id : Mapped[int] = mapped_column(primary_key=True)
+    fname : Mapped[str] = mapped_column(String(50))
+    lname : Mapped[str] = mapped_column(String(50))
+    email : Mapped[str] = mapped_column(String(100))
+    phone : Mapped[str] = mapped_column(String(100))
+    designation_id : Mapped[str] = mapped_column(ForeignKey('hrms_designation.id'))
+    designation : Mapped['Designation'] = relationship(back_populates = "employees")
 
-    
+class Designation(HRDBBase):
+    __tablename__='hrms_designation'
+    id : Mapped[int] = mapped_column(primary_key=True)
+    designation_name : Mapped[str] = mapped_column(String(100))
+    max_leaves : Mapped[int] = mapped_column(Integer(100))
+    employees : Mapped ["Employee"] = relationship(back_populates ="designation")
 
+class Leave(HRDBBase):
+    __tablename__ = "hrms_leaves"
+    __table_args__ = (        
+        UniqueConstraint("employee_id", "date"),
+        )
+    id: Mapped[int] = mapped_column(primary_key=True)
+    date: Mapped[datetime.date] = mapped_column(Date())
+    employee_id: Mapped[int] = mapped_column(ForeignKey('hrms_employees.id'))
+    reason: Mapped[str] =  mapped_column(String(200))
 
+def create_all(db_uri):
+    logger = logging.getLogger("HR")
+    engine = create_engine(db_uri)
+    HRDBBase.metadata.create_all(engine)
+    logger.info("Created database")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import datetime
-# import logging
-# from typing import List
-
-# from sqlalchemy import String, Integer, Date, create_engine, ForeignKey, UniqueConstraint, select
-# from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, sessionmaker
-
-# class HRDBBase(DeclarativeBase):
-#     def __repr__(self):
-#         return f"{self.__class__.__name__}(id={self.id})"
-
-# class Employee(HRDBBase):
-#     __tablename__ = "hrms_employees"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     fname: Mapped[str] =  mapped_column(String(50))
-#     lname: Mapped[str] =  mapped_column(String(50))
-#     email: Mapped[str] =  mapped_column(String(120))
-#     phone: Mapped[str] =  mapped_column(String(50))
-#     title_id: Mapped[int] = mapped_column(ForeignKey('hrms_designations.id'))
-#     title: Mapped["Designation"] = relationship(back_populates = "employees")
-
-# class Designation(HRDBBase):
-#     __tablename__ = "hrms_designations"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     title: Mapped[str] =  mapped_column(String(100))
-#     max_leaves: Mapped[int] = mapped_column(Integer)
-#     employees: Mapped[List["Employee"]] = relationship(back_populates = "title")
-
-# class Leave(HRDBBase):
-#     __tablename__ = "hrms_leaves"
-#     __table_args__ = (        
-#         UniqueConstraint("employee_id", "date"),
-#         )
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     date: Mapped[datetime.date] = mapped_column(Date())
-#     employee_id: Mapped[int] = mapped_column(ForeignKey('hrms_employees.id'))
-#     reason: Mapped[str] =  mapped_column(String(200))
-
-
-# def create_all(db_uri):
-#     logger = logging.getLogger("HR")
-#     engine = create_engine(db_uri)
-#     HRDBBase.metadata.create_all(engine)
-#     logger.info("Created database")
-
-# def get_session(db_uri):
-#     engine = create_engine(db_uri)
-#     Session = sessionmaker(bind = engine)
-#     session = Session()
-#     return session
+def get_session(db_uri):
+    engine = create_engine(db_uri)
+    Session = sessionmaker(bind = engine)
+    session = Session()
+    return session
