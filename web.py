fa5e1d3ef61fe84591bb5b38ca08e0b8f2b224dc
jishnu <jishnup15@gmail.com>
2023-12-16 16:13:34 +0530

    Imported new library to allow fetching from react app

--- web.py
+++ web.py
@@ -1,13 +1,14 @@
 import db
 import flask
+from flask_cors import CORS
 
-from flask import url_for, redirect,flash
+from flask import url_for, redirect
 from sqlalchemy.sql import func
 
 employees = []
 
 app = flask.Flask("hrms")
-app.secret_key= 'hrms'
+CORS(app)
 d = db.SQLAlchemy(model_class=db.HRDBBase)
 
 @app.route("/", methods=["GET", "POST"])
@@ -20,10 +21,16 @@ def index():
 
 @app.route("/employees")
 def employees():
-    query = d.select(db.Employee).order_by(db.Employee.fname,db.Employee.lname)
+    query = d.select(db.Employee).order_by(db.Employee.id,db.Employee.fname,db.Employee.lname)
     users = d.session.execute(query).scalars()
-    return flask.render_template("userlist.html", users = users)
-
+    ret =[]
+    for user in users:
+        details = {"id":user.id,
+            "fname" : user.fname,
+            "lname" : user.lname,
+            "designation":user.title.title}
+        ret.append(details)
+    return flask.jsonify(ret)
 
 @app.route("/employees/<int:empid>")
 def employee_details(empid):
@@ -45,22 +52,46 @@ def employee_details(empid):
 def add_leave(empid):
     if flask.request.method == "POST":
         leave_q = d.select(func.count(db.Leave.id)).where(db.Leave.employee_id == empid)
-        leave_taken = d.session.execute(leave_q).scalar()
+        leaves_taken = d.session.execute(leave_q).scalar()
         
         query = d.select(db.Designation.max_leaves).where(db.Employee.id == empid)
         max_leave = d.session.execute(query).scalar()
         
-        if leave_taken >= max_leave:
-            return redirect(url_for("employees"))
+        if leaves_taken >= max_leave:
+            print('dwsxfszdsdfdddddddddd')
+            query = d.select(db.Employee).where(db.Employee.id == empid)
+            user = d.session.execute(query).scalar()
+            return flask.render_template("add_leave.html", user=user, leaves_taken=leaves_taken)
+            # return redirect(url_for("employees"))
+
         else:
             leave_date = flask.request.form.get("leave_date")
             leave_reason = flask.request.form.get("leave_reason")
             leave = db.Leave(date=leave_date, employee_id=empid, reason=leave_reason)
             d.session.add(leave)
             d.session.commit()
-        return redirect(url_for("employees")) 
+            return redirect(url_for("employees"))
 
+# @app.route('/search')
+# def search():
+#     query = request.args.get('query', '').lower()
     
+#     # Execute the query and fetch the users
+#     users = d.query(db.Employee).all()
+    
+#     filtered_users = [
+#         user for user in users 
+#         if query in user.fname.lower() or query in user.lname.lower()
+#     ]
+    
+#     # Create a list of dictionaries for JSON serialization
+#     filtered_user_dicts = [
+#         {'id': user.id, 'fname': user.fname, 'lname': user.lname}
+#         for user in filtered_users
+#     ]
+    
+#     return jsonify(filtered_user_dicts)
+
 @app.route("/about")
 def about():
     return flask.render_template("about.html")
